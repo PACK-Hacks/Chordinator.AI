6d40bc6966524a0837974481296e7262efa4ae13
Charlie Li <charlie.li711935@gmail.com>
2024-01-27 22:24:03 -0500

    dropdown scales added

--- chordinator-ai/src/Piano_Comp.js
+++ chordinator-ai/src/Piano_Comp.js
@@ -1,28 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
 import 'react-piano/dist/styles.css';
+import DropdownMenu from './DropdownMenu';
 
 const Piano_Comp = ({ onKeyPress }) => {
+    const [playedNotes, setPlayedNotes] = useState([]);
+    const [isPianoDisabled, setIsPianoDisabled] = useState(false);
+
     const firstNote = MidiNumbers.fromNote('c3');
-    const lastNote = MidiNumbers.fromNote('f5');
-    const keyboardShortcuts = KeyboardShortcuts.create({
-      firstNote: firstNote,
-      lastNote: lastNote,
-      keyboardConfig: KeyboardShortcuts.HOME_ROW,
-    });
+    const lastNote = MidiNumbers.fromNote('b4');
+    
+    const onPlayNote = (midiNumber) => {
+      if (!isPianoDisabled && playedNotes.length < 4) {
+        setPlayedNotes((prevNotes) => [...prevNotes, midiNumber]);
+  
+        console.log(midiNumber)
+
+        if (playedNotes.length === 3) {
+          setIsPianoDisabled(true);
+        }
+      }
+    };
+
+    const handleGenerateChords = () => {
+      setIsPianoDisabled(false);
+      setPlayedNotes([]);
+    };
    
     return (
-      <Piano
-        noteRange={{ first: firstNote, last: lastNote }}
-        playNote={(midiNumber) => {
-          // Play a given note - see notes below
-        }}
-        stopNote={(midiNumber) => {
-          // Stop playing a given note - see notes below
-        }}
-        width={1000}
-        keyboardShortcuts={keyboardShortcuts}
-      />
+      <div>
+        <DropdownMenu />
+        <Piano
+          noteRange={{ first: firstNote, last: lastNote }}
+          playNote={(midiNumber) => {
+            onPlayNote(midiNumber)
+          }}
+          stopNote={(midiNumber) => {
+            // Stop playing a given note - see notes below
+          }}
+          width={1000}
+          keyboardShortcuts={null}
+        />
+        <div>
+          <p>Played Notes: {playedNotes.map((midiNumber) => MidiNumbers.getAttributes(midiNumber).note.replace(/\d/g, ''))}</p>
+          <button onClick={handleGenerateChords}>Generate Chords</button>
+        </div>
+      </div>
     );
 };
 
