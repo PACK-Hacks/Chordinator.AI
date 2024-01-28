cd3a188af46ae2a5fa4a06ede8e853c5a611e996
Parth Patel <parth6646@gmail.com>
2024-01-28 02:12:57 -0500

    connected front end and back end

---
+++ chordinator-ai/flask_backend/server.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify, send_file
+from validity_function import validity_function as vf
+from rng_good import output_good_midi as midi_generator
+from note_to_num import note_to_num
+from flask_cors import CORS
+import sys
+
+app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
+
+
+
+
+@app.route('/')
+def home():
+    return "hello world"
+
+
+
+@app.route('/api/notes', methods=['POST'])
+def notes():
+    print('Request received')
+    data = request.get_json()
+    note1 = data.get('note1')
+    note2 = data.get('note2')
+    note3 = data.get('note3')
+    note4 = data.get('note4')
+    note_array = [note1,note2,note3,note4]
+    note_array = note_to_num(note_array)
+    midi = midi_generator(note_array)
+    print('running send file command')
+    return send_file(midi, as_attachment=True, download_name='output.mid')
+
+    
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug=True, use_reloader=True)
+
--- chordinator-ai/src/Piano_Comp.js
+++ chordinator-ai/src/Piano_Comp.js
@@ -3,6 +3,7 @@ import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
 import 'react-piano/dist/styles.css';
 import Soundfont from 'soundfont-player';
 import DropdownMenu from './DropdownMenu';
+import axios from 'axios';
 
 
 const Piano_Comp = ({ onKeyPress }) => {
@@ -11,6 +12,12 @@ const Piano_Comp = ({ onKeyPress }) => {
 
     const firstNote = MidiNumbers.fromNote('c3');
     const lastNote = MidiNumbers.fromNote('b4');
+    const [formData, setFormData] = useState({
+      note1: '',
+      note2: '',
+      note3: '',
+      note4: '',
+    });
     
     const onPlayNote = (midiNumber) => {
       if (!isPianoDisabled && playedNotes.length < 4) {
@@ -29,9 +36,33 @@ const Piano_Comp = ({ onKeyPress }) => {
       }
     };
 
-    const handleGenerateChords = () => {
+    const handleGenerateChords = async (e) => {
       setIsPianoDisabled(false);
+      setFormData({
+        note1: playedNotes[0],
+        note2: playedNotes[1],
+        note3: playedNotes[2],
+        note4: playedNotes[3],
+      });
       setPlayedNotes([]);
+
+      e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:5000/api/notes', formData);
+
+
+      if (response.status === 200) {
+        const result = response.data;
+        console.log(result);
+        // Handle the result as needed
+      } else {
+        console.error('Failed to fetch');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+
     };
 
     return (
