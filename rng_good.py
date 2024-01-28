10fe273d8739659ae7f78946f3cb54515dac91ed
Austin Wu <84493656+Austinwu-rgb@users.noreply.github.com>
2024-01-27 22:04:33 -0500

    Generator Done

--- rng_good.py
+++ rng_good.py
@@ -3,6 +3,7 @@ from validity_function import valid_all;
 from validity_function import validity_function;
 from csv_convert import write_to_csv;
 from csv_convert import write_to_csv_triple;
+from backwards import output_midi;
 
 I = [1, 6, 5, 4];
 II = [3, 5, 6, 1];
@@ -10,20 +11,21 @@ III = [2, 4, 6, 5];
 IV = [4, 3, 6, 5];
 V = [1, 6, 3, 5];
 VI = [6, 4, 5, 1];
-VII = [6, 5, 1, 2]; 
-
+VII = [4, 1, 6, 5]; 
 
 
 def gen_note(root):
-    return (root + random.choice([0, 2, 4]) + 7 * random.choice([3, 4]));
+    return (root + random.choice([0, 2, 4]) + (7 * random.choice([3, 4])));
+
 
 def gen_chord(root):
-    chord = [root + 7 * 3];
+    chord = [root + 7 * 3]; 
+    
     i = 0;
     while(i < 3):
         temp_note = gen_note(root);
-        if (temp_note in chord):
-            continue;
+        if (temp_note in chord or temp_note > 35):
+            continue;        
         else:
             chord.append(temp_note);
             i+= 1;
@@ -35,19 +37,24 @@ def gen_chord_prog(root_prog):
         chord_prog.append(gen_chord(root_prog[i]));
     return chord_prog;
 
-def gen_list_progs(amount, root_prog, filename):
-    list_root_progs = [];
-    list_chord_progs = [];
-    list_validations = [];
-    for i in range(amount):
-        temp_chord_prog = gen_chord_prog(root_prog);
+# def gen_list_progs(amount, root_prog, filename):
+#     list_root_progs = [];
+#     list_chord_progs = [];
+#     list_validations = [];
+#     for i in range(amount):
+#         temp_chord_prog = gen_chord_prog(root_prog);
 
-        list_root_progs.append(root_prog);
-        list_chord_progs.append(temp_chord_prog);
-        list_validations.append(validity_function(root_prog, temp_chord_prog)); 
+#         list_root_progs.append(root_prog);
+#         list_chord_progs.append(temp_chord_prog);
+#         list_validations.append(validity_function(root_prog, temp_chord_prog)); 
 
-    write_to_csv_triple(list_root_progs, list_chord_progs, list_validations, filename);
+#     write_to_csv_triple(list_root_progs, list_chord_progs, list_validations, filename);
 
-gen_list_progs(100, VI, "rng_good.csv");
+def output_good_midi(root_prog):
+    output_midi(gen_chord_prog(root_prog));
+
+print('start');
+output_good_midi(I); 
+print('done');
 
 
