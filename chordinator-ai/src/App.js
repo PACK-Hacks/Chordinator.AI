a7d7a79e1170b40c18c1e3d24e27604ddae256e2
Charlie Li <charlie.li711935@gmail.com>
2024-01-27 21:32:01 -0500

    added piano

---
+++ chordinator-ai/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import Piano_Comp from "./Piano_Comp.js"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <div className="App">
+        <h1>Input your four notes</h1>
+        <Piano_Comp/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ chordinator-ai/src/Piano_Comp.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
+import 'react-piano/dist/styles.css';
+
+const Piano_Comp = ({ onKeyPress }) => {
+    const firstNote = MidiNumbers.fromNote('c3');
+    const lastNote = MidiNumbers.fromNote('f5');
+    const keyboardShortcuts = KeyboardShortcuts.create({
+      firstNote: firstNote,
+      lastNote: lastNote,
+      keyboardConfig: KeyboardShortcuts.HOME_ROW,
+    });
+   
+    return (
+      <Piano
+        noteRange={{ first: firstNote, last: lastNote }}
+        playNote={(midiNumber) => {
+          // Play a given note - see notes below
+        }}
+        stopNote={(midiNumber) => {
+          // Stop playing a given note - see notes below
+        }}
+        width={1000}
+        keyboardShortcuts={keyboardShortcuts}
+      />
+    );
+};
+
+export default Piano_Comp;
