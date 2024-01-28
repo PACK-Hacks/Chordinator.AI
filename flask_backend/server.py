13dea4cf3e5507cf8ba8bbe8ceabf88691c82bd5
Austin Wu <84493656+Austinwu-rgb@users.noreply.github.com>
2024-01-28 03:29:38 -0500

    Before Charlie

--- flask_backend/server.py
+++ flask_backend/server.py
@@ -2,8 +2,14 @@ from flask import Flask, request, jsonify, send_file
 from validity_function import validity_function as vf
 from rng_good import output_good_midi as midi_generator
 from note_to_num import note_to_num
+from flask_cors import CORS
+import sys
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
+
+
+
 
 @app.route('/')
 def home():
@@ -11,8 +17,9 @@ def home():
 
 
 
-@app.route('/notes', methods=['POST'])
+@app.route('/api/notes', methods=['POST'])
 def notes():
+    print('Request received')
     data = request.get_json()
     note1 = data.get('note1')
     note2 = data.get('note2')
@@ -20,12 +27,12 @@ def notes():
     note4 = data.get('note4')
     note_array = [note1,note2,note3,note4]
     note_array = note_to_num(note_array)
-    midi = midi_generator(note_array)
-    return send_file(midi, as_attachment=True, download_name='output.mid')
+    midi_generator(note_array)
+    # print('running send file command')
+    # return send_file(midi, as_attachment=True, download_name='output.mid')
 
     
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='localhost', port=5000, debug=True, use_reloader=True)
 
-app.run(port=5000)
